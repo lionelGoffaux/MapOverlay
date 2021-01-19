3dec8d133a40fca2e4207ca2d667d4702e4a1364
lionelGoffaux <lionelg@outlook.be>
2021-01-19 10:24:43 +0100

    first commit

---
+++ src/test/java/be/ac/umons/mapOverlay/MainTest.java
@@ -0,0 +1,13 @@
+package be.ac.umons.mapOverlay;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void trueTest(){
+        assertTrue(true);
+    }
+}
