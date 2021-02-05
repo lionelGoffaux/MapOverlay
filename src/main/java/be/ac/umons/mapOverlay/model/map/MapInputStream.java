3a6fddc6158c4bde922116d003c2bb84be554913
DorianoDellAria <doridellaria@live.be>
2021-02-05 15:44:01 +0100

    fix test mapInputStream Locale

--- src/main/java/be/ac/umons/mapOverlay/model/map/MapInputStream.java
+++ src/main/java/be/ac/umons/mapOverlay/model/map/MapInputStream.java
@@ -2,17 +2,23 @@ package be.ac.umons.mapOverlay.model.map;
 
 import java.io.*;
 import java.util.Scanner;
+import java.util.Locale;
 
 public class MapInputStream {
 
     private Scanner scanner;
 
+
     public MapInputStream(String path) throws FileNotFoundException {
         scanner = new Scanner(new FileReader(path));
+        scanner.useLocale(Locale.US);
     }
 
     private Segment readSegment() throws IOException{
-        double x1 = scanner.nextDouble(), y1 = scanner.nextDouble(), x2 = scanner.nextDouble(), y2 = scanner.nextDouble();
+        double x1 = scanner.nextDouble();
+        double y1 = scanner.nextDouble();
+        double x2 = scanner.nextDouble();
+        double y2 = scanner.nextDouble();
         Segment segment = new Segment(x1, y1, x2, y2);
         return segment;
     }
