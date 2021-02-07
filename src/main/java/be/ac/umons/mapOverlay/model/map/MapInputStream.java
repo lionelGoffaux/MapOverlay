919876b69babbbd717337242df9703596de4ce04
lionelGoffaux <lionelg@outlook.be>
2021-02-07 12:15:25 +0100

    stage + first controller

--- src/main/java/be/ac/umons/mapOverlay/model/map/MapInputStream.java
+++ src/main/java/be/ac/umons/mapOverlay/model/map/MapInputStream.java
@@ -8,22 +8,19 @@ public class MapInputStream {
 
     private Scanner scanner;
 
-
     public MapInputStream(String path) throws FileNotFoundException {
         scanner = new Scanner(new FileReader(path));
         scanner.useLocale(Locale.US);
     }
 
-    private Segment readSegment() throws IOException{
-        double x1 = scanner.nextDouble();
-        double y1 = scanner.nextDouble();
-        double x2 = scanner.nextDouble();
-        double y2 = scanner.nextDouble();
+    private Segment readSegment(){
+        double x1 = scanner.nextDouble(), y1 = scanner.nextDouble();
+        double x2 = scanner.nextDouble(), y2 = scanner.nextDouble();
         Segment segment = new Segment(x1, y1, x2, y2);
         return segment;
     }
 
-    public Map readMap() throws IOException{
+    public Map readMap(){
         Map map = new Map();
         while (scanner.hasNext()) {
             map.addSegment(readSegment());
--- src/main/java/be/ac/umons/mapOverlay/view/IntersectionsFinderView.java
+++ src/main/java/be/ac/umons/mapOverlay/view/IntersectionsFinderView.java
@@ -1,12 +1,13 @@
 package be.ac.umons.mapOverlay.view;
 
+import be.ac.umons.mapOverlay.controller.ButtonController;
 import javafx.scene.layout.BorderPane;
 
 public class IntersectionsFinderView extends BorderPane {
 
-    public IntersectionsFinderView() {
+    public IntersectionsFinderView(ButtonController buttonController) {
         super();
-        this.setCenter(new SegmentView(600, 600));
-        this.setRight(new ControlView());
+        setCenter(new SegmentView(600, 600));
+        setRight(new ControlView(buttonController));
     }
 }
