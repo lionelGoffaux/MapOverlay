2cfc829263db4532c65861bf84bb2a140a496dd8
lionel <lionelg@outlook.be>
2021-03-21 14:45:39 +0100

    test compareX and compareY

--- src/test/java/be/ac/umons/mapOverlay/model/geometry/PointTest.java
+++ src/test/java/be/ac/umons/mapOverlay/model/geometry/PointTest.java
@@ -68,6 +68,52 @@ class PointTest {
         Assertions.assertEquals(-1, point4.compareTo(point1));
     }
 
-    // TODO: compare X and Y
+    @Test
+    public void comparisonXTest(){
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(1, 0);
+        Point p3 = new Point(0, 1);
+        Point p4 = new Point(1, 1);
+
+        Assertions.assertEquals(-1, p1.compareX(p2));
+        Assertions.assertEquals(-1, p1.compareX(p4));
+        Assertions.assertEquals(0, p1.compareX(p3));
+
+        Assertions.assertEquals(1, p2.compareX(p1));
+        Assertions.assertEquals(1, p2.compareX(p3));
+        Assertions.assertEquals(0, p2.compareX(p4));
+
+        Assertions.assertEquals(-1, p3.compareX(p2));
+        Assertions.assertEquals(-1, p3.compareX(p4));
+        Assertions.assertEquals(0, p3.compareX(p1));
+
+        Assertions.assertEquals(1, p4.compareX(p1));
+        Assertions.assertEquals(1, p4.compareX(p3));
+        Assertions.assertEquals(0, p4.compareX(p2));
+    }
+
+    @Test
+    public void comparisonYTest(){
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(1, 0);
+        Point p3 = new Point(0, 1);
+        Point p4 = new Point(1, 1);
+
+        Assertions.assertEquals(-1, p1.compareY(p3));
+        Assertions.assertEquals(-1, p1.compareY(p4));
+        Assertions.assertEquals(0, p1.compareY(p2));
+
+        Assertions.assertEquals(0, p2.compareY(p1));
+        Assertions.assertEquals(-1, p2.compareY(p3));
+        Assertions.assertEquals(-1, p2.compareY(p4));
+
+        Assertions.assertEquals(1, p3.compareY(p2));
+        Assertions.assertEquals(0, p3.compareY(p4));
+        Assertions.assertEquals(1, p3.compareY(p1));
+
+        Assertions.assertEquals(1, p4.compareY(p1));
+        Assertions.assertEquals(0, p4.compareY(p3));
+        Assertions.assertEquals(1, p4.compareY(p2));
+    }
 
 }
